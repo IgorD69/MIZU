469cadf771e863d503abaa1c6cacb87504eed609
IgorD69 <igor.dodan111@gmail.com>
2026-06-12 22:25:42 +0300

    Diferent Pages for Workout / Start

--- Scripts_Sport/workout_scene.gd
+++ Scripts_Sport/workout_scene.gd
@@ -1,29 +1,20 @@
 extends Control
 
-@onready var scroll_container: ScrollContainer = $ScrollContainer
+@onready var cards_node: Control = $CardsNode
 
+@onready var main_cards_scene: ScrollContainer = $CardsNode/MainCards
+@onready var workout_scene: Control = $CardsNode/WorkoutScene
 
 var drag_sensitivity_amount = 0.005
 var current_amount = 0
 
 
 
-#func _ready() -> void:
-	#pass
-#
-#
-#
-#func _input(event):
-	#if event is InputEventScreenDrag:
-		#_handle_vertical_drag(event, )
-		#
-		#
-	#if event.is_action("ui_cancel"):
-		#get_tree().quit()
-#
-#func _handle_vertical_drag(event: InputEventScreenDrag) -> void:
-	#var rel = event.relative
-	#
-	### Vertical Movement
-	#if abs(rel.y) > abs(rel.x):
-		#pass
+func _on_workoun_btn_pressed() -> void:
+	main_cards_scene.visible = false
+	workout_scene.visible = true
+
+
+func _on_home_btn_pressed() -> void:
+	main_cards_scene.visible = true
+	workout_scene.visible = false
